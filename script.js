492af70d8907b1f8423799aae9c96c4fdba7a7dd
kayyla98 <kayla.cayla@yahoo.com>
2026-07-29 21:50:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-style.css
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function() {
+        li.style.textDecoration = "line-through";
+        li.style.opacity = "0.5";
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
